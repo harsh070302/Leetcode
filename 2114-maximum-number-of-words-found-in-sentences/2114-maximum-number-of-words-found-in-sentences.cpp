e6d51eb252d058cd4f66a778f01335c6c2d83679
Harsh Agarwal <77448365+harsh070302@users.noreply.github.com>
2023-02-27 11:06:00 +0530

    Time: 14 ms (56.90%), Space: 10.4 MB (15.48%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
-        int spaces=0, maxcount=0;
-        for(auto a:sentences){
-            spaces=count(a.begin(),a.end(),' ');
-            if(spaces>maxcount)
-                maxcount=spaces;
-        }
+        int maxcount=0;
+        for(auto a:sentences)
+            maxcount=max(maxcount,(int)count(a.begin(),a.end(),' '));
      return maxcount+1;   
     }
 };
