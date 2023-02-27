6b576ed0724a2c12c62d054e7a36415e0111cb1e
Harsh Agarwal <77448365+harsh070302@users.noreply.github.com>
2023-02-27 10:40:43 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (79.13%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        
+        int c=0;
+        
+        for(int i=0;i<jewels.length();i++){
+            c=c+ count(stones.begin(),stones.end(),jewels[i]);
+        }
+        
+        return c;
+    }
+};
