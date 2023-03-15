0609838ebdf89faa9ed7d3e1ff0b3cc025aef4b6
Harsh Agarwal <77448365+harsh070302@users.noreply.github.com>
2023-03-15 18:17:54 +0530

    Time: 11 ms (15.44%), Space: 8.4 MB (93.34%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+         int n=s.length(), left=0, right=n;
+        vector<int> answer(n+1, 0);
+        for( int i=0; i<n ; i++ ){
+            answer[i] = s[i] == 'I' ? left++ : right--;  
+        }
+        answer[n] = left;
+        return answer;
+    }
+};
