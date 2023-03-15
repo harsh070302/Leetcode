d8a701bb80be2e876da854798064c635e66791a6
Harsh Agarwal <77448365+harsh070302@users.noreply.github.com>
2023-03-15 18:14:18 +0530

    Time: 20 ms (12.52%), Space: 10.7 MB (79.75%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& p) {
+          set<string> s;
+		
+        for(auto &x:p)s.insert(x[1]); 
+		
+        for(auto &x:p)s.erase(x[0]);
+        return *s.begin();
+    }
+};
