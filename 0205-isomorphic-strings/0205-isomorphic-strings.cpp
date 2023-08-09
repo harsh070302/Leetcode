d2194e49cf5242b0a87eb8db59813686773dc373
Harsh Agarwal <77448365+harsh070302@users.noreply.github.com>
2023-08-09 13:09:43 +0530

    Time: 7 ms (67.88%), Space: 7.2 MB (12.81%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size() != t.size())
+            return false;
+        
+        unordered_map<char,char>mps;
+        unordered_map<char,char>mpt;
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(mps[s[i]] && mps[s[i]] != t[i]) return false;
+            if(mpt[t[i]] && mpt[t[i]] != s[i]) return false;
+
+            mps[s[i]] = t[i];
+            mpt[t[i]] = s[i];
+        }
+        return true;
+
+    }
+};
